8d8f6dea7ee2630d93f80022598d633b12f20188
bluers <bluers@users.noreply.github.com>
2020-06-17 15:37:46 +0800

    编码问题
    编码应当与目标Oracle上执行的编码相同

--- oracle_wrap.sh
+++ oracle_wrap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+export LANG="en_US.UTF-8"
+export NLS_LANG="AMERICAN_AMERICA.ZHS16GBK"
 basepath=`pwd`
 srcpath=$basepath/source
 tarpath=$basepath/target
-echo "">$basepath/wrap.log
+//echo "">$basepath/wrap.log
+echo $LANG
+echo $NLS_LANG
 total=`ls $srcpath | wc -l`
-for i in `ls $srcpath`;do
+for i in `ls $srcpath`;do 
 echo $i
-wrap iname=$srcpath/$i oname=$tarpath/$i >> $basepath/wrap.log
+wrap iname=$srcpath/$i oname=$tarpath/$i >> $basepath/wrap.log 
 sleep 0.2;
-done;
+done; 
